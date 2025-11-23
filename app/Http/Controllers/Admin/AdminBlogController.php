e38ac8a6ee7bdd245e76944e09ebe27b801c1a8f
nao-United92 <naoyauver@icloud.com>
2025-11-23 11:16:40 +0900

    feat: ブログ管理 編集・更新・削除機能の実装

--- app/Http/Controllers/Admin/AdminBlogController.php
+++ app/Http/Controllers/Admin/AdminBlogController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\StoreBlogRequest;
+use App\Http\Requests\Admin\UpdateBlogRequest;
 use App\Models\Blog;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 
 class AdminBlogController extends Controller
@@ -12,7 +14,8 @@ class AdminBlogController extends Controller
     // ブログ一覧画面
     public function index()
     {
-        return view('admin.blogs.index');
+        $blogs = Blog::all();
+        return view('admin.blogs.index', ['blogs' => $blogs]);
     }
 
     // ブログ投稿画面
@@ -40,27 +43,38 @@ class AdminBlogController extends Controller
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+    // 指定したIDのブログの編集画面
     public function edit(string $id)
     {
-        //
+        $blog = Blog::findOrFail($id);
+        return view('admin.blogs.edit', ['blog' => $blog]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    // 指定したIDのブログの更新処理
+    public function update(UpdateBlogRequest $request, string $id)
     {
-        //
+        $blog = Blog::findOrFail($id);
+        $updateData = $request->validated();
+
+        // 画像を変更する場合
+        if($request->has('image')) {
+            // 変更前の画像を削除
+            Storage::disk('public')->delete($blog->image);
+            // 変更後の画像をアップロード、保存パスを更新対象データにセット
+            $updateData['image'] = $request->file('image')->store('blogs', 'public');
+        }
+        $blog->update($updateData);
+
+        return to_route('admin.blogs.index')->with('success', 'ブログを更新しました');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+    // 指定したIDのブログの削除処理
     public function destroy(string $id)
     {
-        //
+        $blog = Blog::findOrFail($id);
+        $blog->delete();
+        Storage::disk('public')->delete($blog->image);
+
+        return to_route('admin.blogs.index')->with('success', 'ブログを削除しました');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,6 @@ Route::get('/contact/complete', [ContactController::class, 'complete'])->name('c
 Route::get('/admin/blogs', [AdminBlogController::class, 'index'])->name('admin.blogs.index');
 Route::get('/admin/blogs/create', [AdminBlogController::class, 'create'])->name('admin.blogs.create');
 Route::post('//admin/blogs/store', [AdminBlogController::class, 'store'])->name('admin.blogs.store');
+Route::get('/admin/blogs/{blog}', [AdminBlogController::class, 'edit'])->name('admin.blogs.edit');
+Route::put('/admin/blogs/{blog}', [AdminBlogController::class, 'update'])->name('admin.blogs.update');
+Route::delete('/admin/blogs/{blog}', [AdminBlogController::class, 'destroy'])->name('admin.blogs.destroy');
