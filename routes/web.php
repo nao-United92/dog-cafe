502684ad3db22a9f58715c26e3ef74c5c20e7cfc
nao-United92 <naoyauver@icloud.com>
2025-12-19 10:42:09 +0900

    add login function and set login routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\ContactController;
-use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\AdminBlogController;
+use App\Http\Controllers\Admin\AuthController;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'index');
@@ -23,3 +24,7 @@ Route::delete('/admin/blogs/{blog}', [AdminBlogController::class, 'destroy'])->n
 // ユーザー管理
 Route::get('/admin/users/create', [UserController::class, 'create'])->name('admin.users.create');
 Route::post('/admin/users', [UserController::class, 'store'])->name('admin.users.store');
+
+// 認証
+Route::get('/admin/login', [AuthController::class, 'showLoginForm'])->name('admin.login');
+Route::post('/admin/login', [AuthController::class, 'login']);
